201743262a1c0e23b21623d71d7bb251ea904eac
Tim Fletcher <timfletcher88@gmail.com>
2018-08-07 11:15:00 +1000

    merged in latest features changes

--- diis_theme_container/diis_theme/js/scripts.js
+++ diis_theme_container/diis_theme/js/scripts.js
@@ -770,21 +770,27 @@
     } // End printDetection
 
 
-    // Detect images with no native Alt text, and dynamically retrieve
-    // it from another field. This applies to images inserted via
-    // Drupal image styles and revealed with Display Suite. Who knows.
+    // Detect images with no native Alt text, and add the attribute then 
+    // dynamically retrieve it from another field. This applies to images
+    // inserted via Drupal image styles and revealed with Display Suite.
     Drupal.behaviors.imageAltText = {
         attach: function(context, settings) {
 
-            // NOTE: this alt text is a custom field that uses Display Suite. 
+            // NOTE: .ext-alt-text is a custom field that uses Display Suite. 
             // If the field is not rendered to the page when an image is 
-            // displayed, this won't work for that image.
-            var alt = $('.ext-alt-text');
-
-            $.each(alt, function() {
-                var img = $(this).next('img');
-
-                img.attr('alt', $(this).text());
+            // displayed, this snippet won't work for that image.
+            var extAltText = $('.ext-alt-text'),
+                imgNoAltText = $('img:not(img[alt])');
+               
+            // If any image has no 'alt' attribute at all (not including empty),
+            // add one for accessibility
+           $.each(imgNoAltText, function() {
+                $(this).attr('alt', '');
+            });
+            
+            // 
+            $.each(extAltText, function() {
+                $(this).next('img').attr('alt', $(this).text());
                 $(this).remove();
             });
         }
